377851c6ab0ab6703340864a1e740516a38bc772
Ritesh Kumar Singh <talktoritesh67@gmail.com>
2025-02-11 23:15:18 +0530

    Fixed homepage loading issue

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const NoPage = lazy(() => import("./layout/NoPage"));
 
 function App() {
   return (
-    <Router>
+    <Router basename="/scholar-bridge-academy">
       <Suspense fallback={<Preloader />}>
         <Routes>
           <Route path="" element={<Home />} />  {/* ✅ Remove / from path */}
