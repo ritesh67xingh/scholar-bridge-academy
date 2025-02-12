126f37dc2a7a1726513d0134bc107129d24b1794
Ritesh Kumar Singh <talktoritesh67@gmail.com>
2025-02-12 23:33:47 +0530

    homepage update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Preloader from "./layout/Preloader";
